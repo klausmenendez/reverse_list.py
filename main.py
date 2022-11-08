8646c727fc8c32ecb261bf0eb870453fcd641753
klausmenendez <menendek@oregonstate.edu>
2022-11-07 21:51:57 -0800

    Initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def reverse_list(vals):
+    print(vals[::-1])
+
+#reverse_list([1,3,5,7])
