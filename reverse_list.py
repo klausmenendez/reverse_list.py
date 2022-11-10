053e479dab899a9d98435108af950aea48896311
klausmenendez <114318894+klausmenendez@users.noreply.github.com>
2022-11-09 21:29:24 -0800

    Update reverse_list.py

--- reverse_list.py
+++ reverse_list.py
@@ -2,10 +2,11 @@
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
+'''
+Returns the inputted list as backwards by setting the origional list eqaul to a backwards copy
+'''
 def reverse_list(vals):
     
-    print(vals[::-1])
+    vals[::1]=vals[::-1]
 
 #reverse_list([1,3,5,7])
